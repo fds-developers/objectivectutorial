bb366495c54cc89d3c33a13bb79e85b8ef283659
mickaelgermemont osx <mickaelgermemont@gmail.com>
2020-12-29 21:35:25 +0100

    init

---
+++ objectivectutorial/objectivectutorial/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objectivectutorial
+//
+//  Created by MICKAEL GERMEMONT on 12/29/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    
+    return 0;
+}
